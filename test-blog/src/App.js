aab5eaf6d8e166f8dc9644765cfd989efd467d25
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-12-05 21:18:03 +0000

    set up

---
+++ test-blog/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className='App'>
+            <h1>Practice Blog</h1>
+            <div id='page-body'>Welcome to this practice blog!</div>
+        </div>
+    );
+}
+
+export default App;
