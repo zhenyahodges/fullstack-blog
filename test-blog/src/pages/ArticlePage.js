57a278e3fe04c9c90df7a4daa92330c3efee18d0
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-12-05 21:44:47 +0000

    set up-routes

---
+++ test-blog/src/pages/ArticlePage.js
@@ -0,0 +1,5 @@
+const ArticlePage = () => {
+    return <h1>This is the Article Page</h1>;
+};
+
+export default ArticlePage;
---
+++ test-blog/src/pages/ArticlesListPage.js
@@ -0,0 +1,5 @@
+const ArticlesListPage = () => {
+    return <h1>This is the Articles List Page</h1>;
+};
+
+export default ArticlesListPage;
