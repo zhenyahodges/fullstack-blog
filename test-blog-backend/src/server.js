495555cad9926cad015697025d5dbe5eef637f46
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-12-07 14:15:46 +0000

    nodemon setup etc

--- test-blog-backend/src/server.js
+++ test-blog-backend/src/server.js
@@ -1,9 +1,31 @@
 import express from 'express';
 
-const app = express();
+let articlesInfo = [
+    {
+        name: 'learn-react',
+        upvotes: 0,
+comments: [],
+    },
+    {
+        name: 'mongodb',
+        upvotes: 0,
+        comments: [],
+    },
+];
 
-app.get('/hello', (req, res) => {
-    res.send('Hello!');
+const app = express();
+app.use(express.json());
+
+app.put('/api/articles/:name/upvote', (req, res) => {
+    const { name } = req.params;
+    const article = articlesInfo.find((a) => a.name === name);
+
+    if (article) {
+        article.upvotes += 1;
+        res.send(`The ${name} article has ${article.upvotes} upvotes!`);
+    }else{
+        res.send('That article doesn\'t exist');
+    }
 });
 
 app.listen(8000, () => {
